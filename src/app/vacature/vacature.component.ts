622e790b0d10337c422c89ff85acbd3db623f361
Job <admin@chu.mk>
2018-10-30 17:52:58 +0100

    Dialog works on click

--- src/app/vacature/vacature.component.ts
+++ src/app/vacature/vacature.component.ts
@@ -1,58 +1,30 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { MediaMatcher } from '@angular/cdk/layout';
-import { AppComponent } from 'src/app/app.component';
+import { Component } from '@angular/core';
 
 import { VACATURELIST } from './vacatureList';
 
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-vacature',
   templateUrl: './vacature.component.html',
   styleUrls: ['./vacature.component.scss'],
 })
+
 export class VacatureComponent {
   navlist = VACATURELIST;
 
-  // Import global vars
-  newAppComponent: AppComponent = new AppComponent();
-
-  // Navigation JS stuff
-  mobileQuery: MediaQueryList;
-
-  private mobileQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
-    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this.mobileQueryListener);
-  }
-}
-
-export class DialogComponent {
-
   constructor(public dialog: MatDialog) {}
 
-  public openDialog(): void {
-    const dialogRef = this.dialog.open(DialogComponentDialog, {
-      width: '250px',
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      height: '400px',
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(result);
     });
   }
-}
-
-@Component({
-  selector: 'dialog.component',
-  templateUrl: './dialog/dialog.component.html',
-  styleUrls: ['./dialog/dialog.component.scss'],
-})
-export class DialogComponentDialog {
-
-  constructor(
-    public dialogRef: MatDialogRef<DialogComponentDialog>,
-    ) {}
-
-  onNoClick(): void {
-    this.dialogRef.close();
-  }
-
 }

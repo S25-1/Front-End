39a047cc2aa3fe8dfff2f06fefe93f43a1d12754
Job <contact@chu.mk>
2018-11-05 17:51:15 +0100

    Updated travis and testing files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,20 @@ import { AppComponent } from './app.component';
 import { NavfooterModule } from './navfooter/navfooter.module';
 
 import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { AddEmployeeModule } from './add-employee/add-employee.module';
 import { AddVacancyModule } from './add-vacancy/add-vacancy.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     LoginComponent,
   ],
   imports: [
+    DashboardModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -27,6 +27,9 @@ import { environment } from '../environments/environment';
     AddEmployeeModule,
     AddVacancyModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
+  exports: [
+    LoginComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],

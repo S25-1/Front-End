59f07266422059146428ac9afe9f4e3cca92411f
Sebastiaan <11720523+sebasbongers@users.noreply.github.com>
2018-11-07 15:12:04 +0100

    More info on dialog

--- src/app/vacancy/vacancy.component.ts
+++ src/app/vacancy/vacancy.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { VacancyService } from './vacancy.service';
 import { Observable } from 'rxjs';
-import * as moment from 'moment';
 // import { VACANCYLIST } from './vacancyList';
 
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';

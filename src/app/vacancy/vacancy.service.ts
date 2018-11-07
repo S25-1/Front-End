7602dc195ceba2a3f1951140dc7b1376a29c0f89
Job <admin@chu.mk>
2018-11-07 23:37:19 +0100

    reconfigured service worker to work with api

--- src/app/vacancy/vacancy.service.ts
+++ src/app/vacancy/vacancy.service.ts
@@ -5,7 +5,7 @@ import { Vacancy, Skill } from './vacancyListItem';
 
 @Injectable()
 export class VacancyService {
-  private url: string = 'http://cgi-group1.azurewebsites.net/api/vacancy/getVacancyList';
+  private url: string = 'https://cgi-group1.azurewebsites.net/api/vacancy/getVacancyList';
   constructor(private http: HttpClient) {}
 
   getVacancy(): Observable<Vacancy[]> {

d6b3be4f5af34dd300d19dc7dac7d6229383ee46
Sander <sander.vdhulst98@gmail.com>
2018-11-08 11:23:16 +0100

    deleted import

--- src/app/vacancy/vacancy.component.ts
+++ src/app/vacancy/vacancy.component.ts
@@ -1,7 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { VacancyService } from './vacancy.service';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import '@material/chips';
 import { DialogComponent } from './dialog/dialog.component';
 
 @Component({

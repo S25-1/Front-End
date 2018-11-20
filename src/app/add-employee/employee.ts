2c149247be995194e897cf3d0ddac824d3879951
Levi <levicrietee@hotmail.com>
2018-11-20 20:39:25 +0100

    added elements to add-employee

---
+++ src/app/add-employee/employee.ts
@@ -0,0 +1,77 @@
+
+export class Employee {
+    userType: string;
+    name: string;
+    email: string;
+    password: string;
+    dateOfBirth: Date;
+    phoneNumber: string;
+    jobType: JobType;
+    hourly_Wage: string;
+    Skills: Skill[];
+
+    getJobTypes(): JobType[] {
+      return JSON.parse(`
+        { "jobTypes" : [
+          {
+            "value"       : "1",
+            "placeholder" : "stock clerk"
+          },
+          {
+            "value"       : "2",
+            "placeholder" : "cashier"
+          },
+          {
+            "value"       : "3",
+            "placeholder" : "manager"
+          },
+          {
+            "value"       : "4",
+            "placeholder" : "greengrocer"
+          },
+          {
+            "value"       : "5",
+            "placeholder" : "security guard"
+          }
+        ]
+      }`).jobTypes;
+    }
+    getSkills(): Skill[] {
+      return JSON.parse(`
+      { "skills" : [
+        {
+          "value"       : "1",
+          "placeholder" : "first aid"
+        },
+        {
+          "value"       : "2",
+          "placeholder" : "marketing"
+        },
+        {
+          "value"       : "3",
+          "placeholder" : "speaks English"
+        },
+        {
+          "value"       : "4",
+          "placeholder" : "speaks German"
+        },
+        {
+          "value"       : "5",
+          "placeholder" : "stock manager"
+        }
+      ]
+    }`).skills;
+    }
+    // This is a bit hacky but it is easy for testing
+  }
+  
+  export interface JobType {
+    value: string;
+    placeholder: string;
+  }
+  
+  export interface Skill {
+    value: string;
+    placeholder: string;
+  }
+  

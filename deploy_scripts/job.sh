57f731cfcb05d2150e77b5088082f5240b987703
Job <contact@fontys.me>
2018-11-23 19:41:51 +0100

    Testing another deploy script

--- deploy_scripts/job.sh
+++ deploy_scripts/job.sh
@@ -1,2 +1,5 @@
-cp ./deploy_scripts/firebase/job.json ./firebase.json
+#!/bin/bash
+
+cp -v ${TRAVIS_BUILD_DIR}/deploy_scripts/firebase/job.json ${TRAVIS_BUILD_DIR}/firebase.json
+cd ${TRAVIS_BUILD_DIR}
 firebase deploy --only hosting:job-cgi --token ${FIREBASE_TOKEN}

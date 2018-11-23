68d6a512738fa636d52bc8c0c0f016188612fb21
Job <contact@fontys.me>
2018-11-23 23:00:04 +0100

    Reverted most of the recent travis-ci changes

--- deploy_scripts/job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -v ${TRAVIS_BUILD_DIR}/deploy_scripts/firebase/job.json ${TRAVIS_BUILD_DIR}/firebase.json
-cd ${TRAVIS_BUILD_DIR}
-ng build --prod
-firebase deploy --only hosting:job-cgi --token ${FIREBASE_TOKEN}
--- deploy_scripts/master.sh
+++
@@ -1,2 +0,0 @@
-cp ./firebase/master.json ../firebase.json
-firebase deploy --only hosting:master-cgi --token ${FIREBASE_TOKEN}

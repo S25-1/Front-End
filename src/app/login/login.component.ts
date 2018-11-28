fa8f380165e93c6bc74ef7537223be1670346e45
Job <contact@fontys.me>
2018-11-29 00:02:00 +0100

    Set privacy of authService to public to allow component interaction

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService,
+    public authService: AuthService,
     ) { }
 
   credentialsForm: FormGroup;
@@ -21,8 +21,6 @@ export class LoginComponent implements OnInit {
       email: ['something@mail.com'],
       password: ['sexyAnd@securePass12'],
     });
-
-    console.log(this.authService.isLoggedIn());
   }
 
   login() {

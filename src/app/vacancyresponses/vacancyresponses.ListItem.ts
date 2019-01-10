88f18c39fe02a12b92f76f57224053da3bee559e
Levi <levicrietee@hotmail.com>
2019-01-10 13:54:15 +0100

    Vacancy responses aangepast

--- src/app/vacancyresponses/vacancyresponses.ListItem.ts
+++ src/app/vacancyresponses/vacancyresponses.ListItem.ts
@@ -2,4 +2,9 @@ export interface AcceptedUser {
   userID: number;
   vacancyID: number;
   statusID: number;
+  userStatusName: string;
+  vacancyJobName: string;
+  vacancyDateBegin: Date;
+  vacancyDateEnd: Date;
 }
+
